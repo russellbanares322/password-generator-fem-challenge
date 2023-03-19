a933d31816514d278af624c937caff907c8d8777
russellbanares322 <banaresrussell11@gmail.com>
2023-03-19 12:01:26 +0800

    feat: implement conditional rendering of the strength of generated text depending on range value

--- src/components/PasswordGenerator.jsx
+++ src/components/PasswordGenerator.jsx
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FaRegCopy } from "react-icons/fa";
 import { FiArrowRight } from "react-icons/fi";
 
 const PasswordGenerator = () => {
   const [checkedValue, setCheckedValue] = useState([]);
   const [rangeValue, setRangeValue] = useState(0);
+  const [generatedText, setGeneratedText] = useState([]);
 
   //* Random characters that will be generated
   const characters = [
@@ -14,6 +15,25 @@ const PasswordGenerator = () => {
     "!@#$%^&*()",
   ];
 
+  //TODO If the checked value array is empty then generate random number but if the length > 0 generate characters depending on the values inside of the array//
+
+  useEffect(() => {
+    if (rangeValue > 0) {
+      const randomCharacters =
+        characters[Math.floor(Math.random() * characters.length)];
+      const letterIndex = Math.floor(Math.random() * randomCharacters.length);
+      const uppercaseLetters = randomCharacters[letterIndex].substring(
+        0,
+        rangeValue
+      );
+
+      setGeneratedText([...generatedText, uppercaseLetters]);
+    } else {
+      const storedText = [...generatedText];
+      const removedText = storedText.slice(0, rangeValue);
+      setGeneratedText(removedText);
+    }
+  }, [rangeValue]);
   //* Checkbox options
   const checkboxOptions = [
     {
@@ -55,14 +75,20 @@ const PasswordGenerator = () => {
     }
   };
   return (
-    <div className="flex justify-center items-center">
+    <div className="flex justify-center items-center h-screen">
       <div className="my-[5rem] mx-[1.4rem] w-[27rem] md:m-[7rem] md:w-[23rem]">
         <p className="text-almost-white text-lg text-center font-semibold">
           Password Generator
         </p>
         <div className="flex justify-between items-center mt-5 bg-light-blue h-[3rem] px-5 ">
-          <p className="text-almost-white font-bold text-xl">
-            {rangeValue === 0 && "P4$W0rD!"}
+          <p className="text-almost-white font-bold text-xl flex items-start justify-start">
+            {generatedText.length === 0
+              ? "P4$W0rD!"
+              : generatedText.map((text, idx) => (
+                  <p className="text-white" key={idx}>
+                    {text}
+                  </p>
+                ))}
           </p>
           <FaRegCopy
             className="text-neon-green cursor-pointer hover:text-white transition duration-75"
@@ -101,11 +127,40 @@ const PasswordGenerator = () => {
           <div className="flex justify-between items-center mt-5 bg-dark-blue h-[3rem] px-5 ">
             <p className="text-almost-white text-sm font-bold">STRENGTH</p>
             <div className="flex gap-1">
-              <span className="mr-2 text-white font-bold hidden">MEDIUM</span>
-              <span className="border-2 border-yellow h-5 w-2 bg-yellow"></span>
-              <span className="border-2 border-white h-5 w-2"></span>
-              <span className="border-2 border-white h-5 w-2"></span>
-              <span className="border-2 border-white h-5 w-2"></span>
+              <span className="mr-2 text-white font-bold h-5">
+                {rangeValue === 0 && ""}
+                {rangeValue >= 1 && rangeValue <= 9 && "WEAK"}
+                {rangeValue >= 10 && rangeValue <= 15 && "MEDIUM"}
+                {rangeValue >= 16 && "STRONG"}
+              </span>
+              <span
+                className={`${
+                  rangeValue >= 1
+                    ? "bg-yellow border-2 border-yellow h-5 w-2"
+                    : "border-2 bg-none border-white h-5 w-2"
+                }`}
+              ></span>
+              <span
+                className={`${
+                  rangeValue >= 5
+                    ? "bg-yellow border-2 border-yellow h-5 w-2"
+                    : "border-2 bg-none border-white h-5 w-2"
+                }`}
+              ></span>
+              <span
+                className={`${
+                  rangeValue >= 10
+                    ? "bg-yellow border-2 border-yellow h-5 w-2"
+                    : "border-2 bg-none border-white h-5 w-2"
+                }`}
+              ></span>
+              <span
+                className={`${
+                  rangeValue >= 16
+                    ? "bg-yellow border-2 border-yellow h-5 w-2"
+                    : "border-2 bg-none border-white h-5 w-2"
+                }`}
+              ></span>
             </div>
           </div>
           <div className="grid mt-5">
