1a6efe88c5424714a6c88dc9375d18b64e46892c
russellbanares322 <banaresrussell11@gmail.com>
2023-03-21 21:29:59 +0800

    feat: add function in generate password button

--- src/components/PasswordGenerator.jsx
+++ src/components/PasswordGenerator.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { FaRegCopy } from "react-icons/fa";
-import { FiArrowRight } from "react-icons/fi";
+import { ImArrowRight2 } from "react-icons/im";
 import { toast } from "react-hot-toast";
 
 const PasswordGenerator = () => {
@@ -16,22 +16,6 @@ const PasswordGenerator = () => {
     "1234567890",
     "!@#$%^&*()",
   ];
-
-  const uppercaseCharacters = characters[0];
-  const lowercaseCharacters = characters[1];
-  const numbers = characters[2];
-  const symbols = characters[3];
-
-  const allCharacters =
-    characters[Math.floor(Math.random() * characters.length)];
-  const randomCharacters =
-    allCharacters[Math.floor(Math.random() * allCharacters.length)];
-  const randomUppercase =
-    uppercaseCharacters[Math.floor(Math.random() * uppercaseCharacters.length)];
-  const randomLowercase =
-    lowercaseCharacters[Math.floor(Math.random() * lowercaseCharacters.length)];
-  const randomNumbers = numbers[Math.floor(Math.random() * numbers.length)];
-  const randomSymbols = symbols[Math.floor(Math.random() * symbols.length)];
 
   //* Checkbox options
   const checkboxOptions = [
@@ -63,23 +47,7 @@ const PasswordGenerator = () => {
   };
 
   const handleRangeInput = (e) => {
-    if (rangeValue < e.target.value) {
-      if (checkedValue.includes("uppercase")) {
-        setGeneratedText([...generatedText, randomUppercase]);
-      }
-      if (checkedValue.includes("lowercase")) {
-        setGeneratedText([...generatedText, randomLowercase]);
-      }
-      if (checkedValue.includes("numbers")) {
-        setGeneratedText([...generatedText, randomNumbers]);
-      }
-      if (checkedValue.includes("symbols")) {
-        setGeneratedText([...generatedText, randomSymbols]);
-      }
-      if (checkedValue.length === 0 || checkedValue.length === 4) {
-        setGeneratedText([...generatedText, randomCharacters]);
-      }
-    } else {
+    if (rangeValue > e.target.value) {
       const storedText = [...generatedText];
       storedText.pop();
       setGeneratedText(storedText);
@@ -98,13 +66,66 @@ const PasswordGenerator = () => {
   };
 
   //* Password generator button
-  const handleCopy = async () => {
+  const handleGeneratePassword = () => {
+    let generatedPassword;
+    if (rangeValue > 0) {
+      if (checkedValue.includes("uppercase")) {
+        generatedPassword = [...Array.from({ length: rangeValue })]
+          .map(
+            () =>
+              characters[0][Math.floor(Math.random() * characters[0].length)]
+          )
+          .join("");
+      }
+      if (checkedValue.includes("lowercase")) {
+        generatedPassword = [...Array.from({ length: rangeValue })]
+          .map(
+            () =>
+              characters[1][Math.floor(Math.random() * characters[1].length)]
+          )
+          .join("");
+      }
+      if (checkedValue.includes("numbers")) {
+        generatedPassword = [...Array.from({ length: rangeValue })]
+          .map(
+            () =>
+              characters[2][Math.floor(Math.random() * characters[2].length)]
+          )
+          .join("");
+      }
+      if (checkedValue.includes("symbols")) {
+        generatedPassword = [...Array.from({ length: rangeValue })]
+          .map(
+            () =>
+              characters[3][Math.floor(Math.random() * characters[3].length)]
+          )
+          .join("");
+      }
+      if (checkedValue.length === 0 || checkedValue.length === 4) {
+        generatedPassword = [...Array.from({ length: rangeValue })]
+          .map(
+            () =>
+              characters[Math.floor(Math.random() * characters.length)][
+                Math.floor(Math.random() * characters.length)
+              ]
+          )
+          .join("");
+      }
+      setGeneratedText(generatedPassword.split(""));
+    } else {
+      toast.error("Please select character length");
+    }
+  };
+  console.log(generatedText);
+
+  //* Copy to clipboard
+  const handleCopyPassword = async () => {
     if (generatedText.length > 0) {
       try {
         await navigator.clipboard.writeText(generatedText.join(""));
         toast.success("Copied to clipboard");
       } catch (err) {
-        console.log(err);
+        toast.error("Failed to copy, please try again");
       }
     } else {
       toast.error("Input text is empty");
@@ -118,25 +139,22 @@ const PasswordGenerator = () => {
         </p>
         <div className="flex justify-between items-center mt-5 bg-light-blue h-[3rem] px-5 ">
           <p className="text-almost-white font-bold text-xl flex items-start justify-start">
-            {generatedText.length === 0
-              ? "P4$W0rD!"
-              : generatedText.length > 0 &&
-                generatedText.map((text, idx) => (
-                  <span className="text-white" key={idx}>
-                    {text}
-                  </span>
-                ))}
+            {generatedText.length === 0 ? (
+              "P4$W0rD!"
+            ) : (
+              <span className="text-white">{generatedText}</span>
+            )}
           </p>
           <FaRegCopy
-            onClick={handleCopy}
+            onClick={handleCopyPassword}
             className="text-neon-green cursor-pointer hover:text-white transition duration-75"
             size={17}
           />
         </div>
         <div className="bg-light-blue mt-5 p-5">
           <div className="flex items-center justify-between">
-            <p className="text-white text-md">Character Length</p>
-            <p className="text-neon-green text-xl">{rangeValue}</p>
+            <p className="text-white text-sm">Character Length</p>
+            <p className="text-neon-green text-2xl">{rangeValue}</p>
           </div>
           <input
             min={0}
@@ -165,8 +183,8 @@ const PasswordGenerator = () => {
           </div>
           <div className="flex justify-between items-center mt-5 bg-dark-blue h-[3rem] px-5 ">
             <p className="text-almost-white text-sm font-bold">STRENGTH</p>
-            <div className="flex gap-1">
-              <span className="mr-2 text-white font-bold h-5">
+            <div className="h-5 flex gap-1 items-center justify-center">
+              <span className="mr-2 text-white font-bold text-lg">
                 {rangeValue === 0 && ""}
                 {rangeValue >= 1 && rangeValue <= 9 && "WEAK"}
                 {rangeValue >= 10 && rangeValue <= 15 && "MEDIUM"}
@@ -203,9 +221,12 @@ const PasswordGenerator = () => {
             </div>
           </div>
           <div className="grid mt-5">
-            <button className="border border-neon-green bg-neon-green h-[3rem] font-bold text-dark-blue flex items-center justify-center gap-4 text-sm hover:bg-light-blue hover:text-neon-green transition duration-50">
+            <button
+              onClick={handleGeneratePassword}
+              className="border border-neon-green bg-neon-green h-[3rem] font-bold text-dark-blue flex items-center justify-center gap-3 text-sm hover:bg-light-blue hover:text-neon-green transition duration-50"
+            >
               GENERATE
-              <FiArrowRight size={13} />
+              <ImArrowRight2 size={12} />
             </button>
           </div>
         </div>
